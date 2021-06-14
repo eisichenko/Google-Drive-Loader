b1b041da2ff8f5506776fadb3ef270e17be8edd0
Egor Isichenko <eisichenko@mail.ru>
2021-06-14 22:52:03 +0300

    deleted branch

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 PAGESIZE = 1000
 
-# LOCAL_DIRECTORY = '/mnt/0E5454BB5454A6ED/programming/SomePrograms/Python/GoogleDriveLoader/files'
-# DRIVE_DIRECTORY = 'test'
+LOCAL_DIRECTORY = '/mnt/0E5454BB5454A6ED/programming/SomePrograms/Python/GoogleDriveLoader/files'
+DRIVE_DIRECTORY = 'test'
 
-LOCAL_DIRECTORY = '/home/egor/Music/audios'
-DRIVE_DIRECTORY = 'audios'
+# LOCAL_DIRECTORY = '/home/egor/Music/audios'
+# DRIVE_DIRECTORY = 'audios'
