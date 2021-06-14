765f9b0c6803d1ccc08ae7a7fc250cfbb8691f0b
Egor Isichenko <eisichenko@mail.ru>
2021-06-14 20:09:11 +0300

    created basic application

---
+++ api_helper.py
@@ -0,0 +1,202 @@
+from gdloader import creds, EXECUTION_RESULT
+from googleapiclient import discovery
+from httplib2 import Http
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+from config import *
+import io
+from threading import Lock
+import local_helper
+
+print_lock = Lock()
+
+
+class bcolors:
+    GREEN = '\033[92m'
+    FAIL = '\033[91m'
+    CYAN = '\033[96m'
+    WARNING = '\033[93m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+
+
+def lock_print(*a, **b):
+    """Thread safe print function"""
+    with print_lock:
+        print(*a, **b)
+
+
+def print_success(string):
+    lock_print(bcolors.GREEN + bcolors.BOLD + str(string) + bcolors.ENDC)
+
+
+def print_fail(string):
+    lock_print(bcolors.FAIL + bcolors.BOLD + str(string) + bcolors.ENDC)
+
+
+def print_cyan(string):
+    lock_print(bcolors.CYAN + bcolors.BOLD + str(string) + bcolors.ENDC)
+
+
+def print_warning(string):
+    lock_print(bcolors.WARNING + bcolors.BOLD + str(string) + bcolors.ENDC)
+
+
+def get_all_files():
+    
+    lock_print('\nGetting all files...\n')
+
+    DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))
+    response = DRIVE.files().list(q='trashed=false', pageSize=PAGESIZE).execute()
+    
+    files = response.get('files', [])
+    nextPageToken = response.get('nextPageToken')
+
+    while nextPageToken:
+        response = DRIVE.files().list(q='trashed=false', pageSize=PAGESIZE, pageToken=nextPageToken).execute()
+        nextPageToken = response.get('nextPageToken')
+        files.extend(response.get('files', []))
+    
+    return files
+
+
+def upload_file(target_name, source_name, folder_name=None):
+    try:
+        if folder_name != None:
+            folder_id = _get_id_by_name(folder_name)
+            file_metadata = { 'name': target_name,
+                              'parents': [folder_id]
+                            }
+        else:
+            file_metadata = { 'name': target_name }
+        
+        lock_print(f'\nUploading file {source_name} --> {target_name}...\n')
+        
+        media = MediaFileUpload(source_name,
+                                resumable=True)
+        
+        DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))
+        file = DRIVE.files().create(body=file_metadata, media_body=media, fields='id, name').execute()
+        
+        if file:
+            print_success(f'File {file["name"]} UPLOADED (id {file["id"]})')
+        else:
+            raise Exception('Could not upload file')
+    except Exception as ex:
+        print_fail(f'File {source_name} FAIL UPLOAD')
+        print_fail(ex)
+        global EXECUTION_RESULT
+        EXECUTION_RESULT = False
+        
+
+def get_folder_items(name):
+    try:
+        lock_print(f'\nGetting files from folder {name}...\n')
+        
+        folder_id = _get_id_by_name(name)
+        
+        DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))
+        response = DRIVE.files().list(q=f"'{folder_id}' in parents and mimeType != 'application/vnd.google-apps.folder' and trashed=false", 
+                        pageSize=PAGESIZE).execute()
+
+        files = response['files']
+        nextPageToken = response.get('nextPageToken')
+        
+        while nextPageToken:
+            response = DRIVE.files().list(q=f"'{folder_id}' in parents and mimeType != 'application/vnd.google-apps.folder' and trashed=false", 
+                            pageSize=PAGESIZE, pageToken=nextPageToken).execute()
+            nextPageToken = response.get('nextPageToken')
+            files.extend(response.get('files', []))
+        
+        return files
+
+    except Exception as ex:
+        print_fail(ex)
+        global EXECUTION_RESULT
+        EXECUTION_RESULT = False
+        
+
+def download_file(file_id, filename):
+    lock_print(f'\nDownloading file {filename}...', end=' ')
+    
+    DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))    
+    request = DRIVE.files().get_media(fileId=file_id)
+    
+    fh = io.BytesIO()
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        lock_print(f'{status.progress() * 100 : .2f}%', end=' ')
+    
+    with io.open(filename, 'wb') as f:
+        fh.seek(0)
+        f.write(fh.read())
+
+    lock_print()
+        
+    if done:
+        print_success(f'Downloaded {filename}')
+    else:
+        print_fail(f'FAILED download {filename}')
+        global EXECUTION_RESULT
+        EXECUTION_RESULT = False
+        
+
+def delete_file(file_id, filename):
+    try:
+        lock_print(f'\nDeleting file {filename}...')
+        
+        DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))
+        DRIVE.files().delete(fileId=file_id).execute()
+        
+        print_success(f'File {filename} was successfully deleted!')
+    except Exception as ex:
+        print_fail(ex)
+        print_fail(f'FAILED to delete file {filename}')
+        global EXECUTION_RESULT
+        EXECUTION_RESULT = False
+
+
+def _get_id_by_name(name):
+    DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))
+    response = DRIVE.files().list(q=f"name='{name}' and trashed=false").execute()
+    
+    if len(response['files']) == 0:
+        raise Exception(f'Name was not found "{name}"')
+    
+    if len(response['files']) > 1:
+        raise Exception(f'Duplicate names "{name}"')
+    return response['files'][0]['id']
+
+
+def get_item_by_name(files, name):
+    files = list(filter(lambda x: x['name'] == name, files))
+    if len(files) == 0:
+        raise Exception(f'Item {name} was not found')
+    if len(files) > 1:
+        print(files)
+        raise Exception(f'Duplicate {name} items')
+    
+    return files[0]
+
+
+def fetch_from_local_to_drive(drive_names_set, local_names_set):
+    lock_print('Fetching for push...\n')
+    upload = local_names_set - drive_names_set
+    delete = drive_names_set - local_names_set    
+    return upload, delete
+
+
+def test_number_of_items(drive_directory, local_directory):
+    drive_items = get_folder_items(drive_directory)
+    local_items_names = local_helper.get_local_directory_files(local_directory)
+    res = len(drive_items) == len(local_items_names)
+    
+    if res:
+        print_success(f'{len(drive_items)} (drive) == {len(local_items_names)} (local)')
+    else:
+        print_fail(f'{len(drive_items)} != {len(local_items_names)}')
+        global EXECUTION_RESULT
+        EXECUTION_RESULT = False
+
+    return res
---
+++ gdloader.py
@@ -0,0 +1,107 @@
+from __future__ import print_function
+from googleapiclient import discovery
+from httplib2 import Http
+from oauth2client import file, client, tools
+from config import *
+import api_helper
+import local_helper
+from os.path import join
+import time
+import threading
+
+EXECUTION_RESULT = True
+
+SCOPES = 'https://www.googleapis.com/auth/drive'
+store = file.Storage('storage.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('client_id.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+DRIVE = discovery.build('drive', 'v3', http=creds.authorize(Http()))
+
+
+if __name__ == '__main__':
+    try:
+        drive_items = api_helper.get_folder_items(DRIVE_DIRECTORY)
+        
+        drive_items_names = list(map(lambda x : x['name'], drive_items))
+        
+        drive_names_set = set(drive_items_names)
+        
+        if len(drive_items_names) != len(drive_names_set):
+            raise Exception('Duplicate drive items')
+        
+        local_items_names = local_helper.get_local_directory_files(LOCAL_DIRECTORY)
+        
+        local_names_set = set(local_items_names)
+        
+        if len(local_items_names) != len(local_names_set):
+            raise Exception('Duplicate local items')
+        
+        upload, delete = api_helper.fetch_from_local_to_drive(drive_names_set=drive_names_set,
+                                             local_names_set=local_names_set)
+        
+        if len(upload) == 0 and len(delete) == 0:
+            if api_helper.test_number_of_items(drive_directory=DRIVE_DIRECTORY, 
+                                            local_directory=LOCAL_DIRECTORY):
+                api_helper.print_success('\nEverything is up to date\n')
+            else:
+                api_helper.print_fail('\nSomething went wrong\n')
+            exit()
+        
+        api_helper.print_cyan('=' * 40)
+        
+        api_helper.print_cyan('Will be uploaded:\n')
+        
+        for f in upload:
+            api_helper.print_cyan(f)
+        
+        api_helper.print_cyan('=' * 40)
+        
+        api_helper.print_warning('\n' + '=' * 40)
+        
+        api_helper.print_warning('Will be deleted:\n')
+        
+        for f in delete:
+            api_helper.print_warning(f)
+            
+        api_helper.print_warning('=' * 40 + '\n')
+
+        choice = input('Are you sure to complete the operation? (y/n) ')
+        
+        if choice == 'y':
+            start = time.time()
+            
+            threads = []
+            
+            for name in upload:
+                threads.append(threading.Thread(target=api_helper.upload_file,
+                                                kwargs={'target_name': name,
+                                                        'source_name': join(LOCAL_DIRECTORY, name), 
+                                                        'folder_name': DRIVE_DIRECTORY}))
+                
+            for name in delete:
+                item = api_helper.get_item_by_name(files=drive_items, name=name)
+                threads.append(threading.Thread(target=api_helper.delete_file,
+                                                kwargs={'file_id': item['id'],
+                                                        'filename': item['name']}))
+
+            for thread in threads:
+                thread.start()
+                
+            for thread in threads:
+                thread.join()
+                
+            api_helper.print_cyan(f'\nOperation took {time.time() - start : .2f} seconds')
+            
+            if api_helper.test_number_of_items(drive_directory=DRIVE_DIRECTORY, 
+                                            local_directory=LOCAL_DIRECTORY) and EXECUTION_RESULT:
+                api_helper.print_success('\nPush was done successfully!!!\n')
+            else:
+                api_helper.print_fail('Push was not completed :(')
+        else:
+            api_helper.print_fail('\nPush declined.\n')
+
+    except Exception as ex:
+        api_helper.print_fail(ex)
+        api_helper.print_fail('Push was not completed :(')
